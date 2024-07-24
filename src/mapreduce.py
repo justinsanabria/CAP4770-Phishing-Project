b00fe037f8207df53c06883beaf4b6902512c326
justinsanabria <justinsanabria11@gmail.com>
2024-07-24 18:54:48 -0400

    Added data and basic mapreduce source code

---
+++ src/mapreduce.py
@@ -0,0 +1,15 @@
+def map_function(email):
+    # Tokenize email and return word, count pairs
+    pass
+
+def reduce_function(word, counts):
+    # counts for each word
+    pass
+
+def main():
+    # Load dataset, run map and reduce, and save results
+    pass
+
+if __name__ == "__main__":
+    main()
+
